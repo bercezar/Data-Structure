bddf79b0dd8431a6f0a9a49b63feab7c076bc924
Bernardo Cezar <113948200+bercezar@users.noreply.github.com>
2025-03-13 11:39:04 -0300

    Create Produto.java

---
+++ produtos/Produto.java
@@ -0,0 +1,13 @@
+package produtos;
+
+public class Produto {
+    int codigo;
+    String nome;
+    int quantidade;
+
+    public Produto(int codigo, String nome, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+}
